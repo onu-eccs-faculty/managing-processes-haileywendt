13e7e5021d53d20a23aaaf234750e201bd231e72
Hailey Wendt <h-wendt@onu.edu>
2024-04-10 22:23:20 -0400

    I've got 2/3 passing

--- shell.c
+++ shell.c
@@ -14,18 +14,16 @@
 /* DEFINE SECTION */
 #define SHELL_BUFFER_SIZE 256   /* Size of the Shell input buffer        */
 #define SHELL_MAX_ARGS 8        /* Maximum number of arguments parsed    */
+#define SHELL_HISTORY_SIZE 9
 
 /* VARIABLE SECTION */
 enum { STATE_SPACE, STATE_NON_SPACE };	/* Parser states */
 
-
-int imthechild(const char *path_to_exec, char *const args[])
-{
-	return execv(path_to_exec, args) ? -1 : 0;
+int imthechild(const char *path_to_exec, char *const args[]) {
+    return execvp(path_to_exec, args) ? -1 : 0;
 }
 
-void imtheparent(pid_t child_pid, int run_in_background)
-{
+void imtheparent(pid_t child_pid, int run_in_background) {
 	int child_return_val, child_error_code;
 
 	/* fork returned a positive pid so we are the parent */
@@ -37,7 +35,10 @@ void imtheparent(pid_t child_pid, int run_in_background)
 		        "  Parent says 'run_in_background=1 ... so we're not waiting for the child'\n");
 		return;
 	}
-	wait(&child_return_val);
+	
+	waitpid(child_pid, &child_return_val, 0); // waitpid
+	// wait(&child_return_val); -> original
+	
 	/* Use the WEXITSTATUS to extract the status code from the return value */
 	child_error_code = WEXITSTATUS(child_return_val);
 	fprintf(stderr,
@@ -52,98 +53,93 @@ void imtheparent(pid_t child_pid, int run_in_background)
 }
 
 /* MAIN PROCEDURE SECTION */
-int main(int argc, char **argv)
-{
-	pid_t shell_pid, pid_from_fork;
-	int n_read, i, exec_argc, parser_state, run_in_background;
-	/* buffer: The Shell's input buffer. */
-	char buffer[SHELL_BUFFER_SIZE];
-	/* exec_argv: Arguments passed to exec call including NULL terminator. */
-	char *exec_argv[SHELL_MAX_ARGS + 1];
+int main(int argc, char **argv) {
+    pid_t shell_pid, pid_from_fork;
+    int n_read, i, exec_argc, parser_state, run_in_background;
+    char buffer[SHELL_BUFFER_SIZE];
+    char *exec_argv[SHELL_MAX_ARGS + 1];
 
-	/* Entrypoint for the testrunner program */
+    int command_counter = 1;
+    char** shell_history[SHELL_HISTORY_SIZE][SHELL_BUFFER_SIZE];
+    int shell_history_next_index = 0;
+    
+    /* Entrypoint for the testrunner program */
 	if (argc > 1 && !strcmp(argv[1], "-test")) {
 		return run_smp1_tests(argc - 1, argv + 1);
 	}
-
-	/* Allow the Shell prompt to display the pid of this process */
+	
 	shell_pid = getpid();
 
-	while (1) {
-	/* The Shell runs in an infinite loop, processing input. */
+    while (1) {
+        fprintf(stdout, "Shell(pid=%d)%i> ", shell_pid, command_counter);
+        fflush(stdout);
 
-		fprintf(stdout, "Shell(pid=%d)> ", shell_pid);
-		fflush(stdout);
+        if (fgets(buffer, SHELL_BUFFER_SIZE, stdin) == NULL)
+            return EXIT_SUCCESS;
+        n_read = strlen(buffer);
+        run_in_background = n_read > 2 && buffer[n_read - 2] == '&';
+        buffer[n_read - run_in_background - 1] = '\n';
 
-		/* Read a line of input. */
-		if (fgets(buffer, SHELL_BUFFER_SIZE, stdin) == NULL)
-			return EXIT_SUCCESS;
-		n_read = strlen(buffer);
-		run_in_background = n_read > 2 && buffer[n_read - 2] == '&';
-		buffer[n_read - run_in_background - 1] = '\n';
+        parser_state = STATE_SPACE;
+        for (exec_argc = 0, i = 0;
+             (buffer[i] != '\n') && (exec_argc < SHELL_MAX_ARGS); i++) {
+            if (!isspace(buffer[i])) {
+                if (parser_state == STATE_SPACE)
+                    exec_argv[exec_argc++] = &buffer[i];
+                parser_state = STATE_NON_SPACE;
+            } 
+            else {
+                buffer[i] = '\0';
+                parser_state = STATE_SPACE;
+            }
+        }
 
-		/* Parse the arguments: the first argument is the file or command *
-		 * we want to run.                                                */
+        buffer[i] = '\0';
 
-		parser_state = STATE_SPACE;
-		for (exec_argc = 0, i = 0;
-		     (buffer[i] != '\n') && (exec_argc < SHELL_MAX_ARGS); i++) {
+        if (!exec_argc)
+            continue;
 
-			if (!isspace(buffer[i])) {
-				if (parser_state == STATE_SPACE)
-					exec_argv[exec_argc++] = &buffer[i];
-				parser_state = STATE_NON_SPACE;
-			} else {
-				buffer[i] = '\0';
-				parser_state = STATE_SPACE;
-			}
-		}
+        exec_argv[exec_argc] = NULL;
 
-		/* run_in_background is 1 if the input line's last character *
-		 * is an ampersand (indicating background execution).        */
+        if (!strcmp(exec_argv[0], "exit")) {
+            printf("Exiting process %d\n", shell_pid);
+            return EXIT_SUCCESS;
+        } 
+        else if (!strcmp(exec_argv[0], "cd") && exec_argc > 1) {
+            if (chdir(exec_argv[1]))
+                fprintf(stderr, "cd: failed to chdir %s\n", exec_argv[1]); 
+        } 
+		else if (!strcmp(exec_argv[0], "sub")) { // Add 'sub' command handling
+            pid_from_fork = fork();
 
+            if (pid_from_fork < 0) {
+                fprintf(stderr, "fork failed\n");
+                continue;
+            }
+            if (pid_from_fork == 0) {
+                return imthechild("./shell", NULL);
+            } 
+            else {
+                imtheparent(pid_from_fork, run_in_background);
+            }
+        }         
+        else {
+            pid_from_fork = fork();
 
-		buffer[i] = '\0';	/* Terminate input, overwriting the '&' if it exists */
-
-		/* If no command was given (empty line) the Shell just prints the prompt again */
-		if (!exec_argc)
-			continue;
-		/* Terminate the list of exec parameters with NULL */
-		exec_argv[exec_argc] = NULL;
-
-		/* If Shell runs 'exit' it exits the program. */
-		if (!strcmp(exec_argv[0], "exit")) {
-			printf("Exiting process %d\n", shell_pid);
-			return EXIT_SUCCESS;	/* End Shell program */
-
-		} else if (!strcmp(exec_argv[0], "cd") && exec_argc > 1) {
-		/* Running 'cd' changes the Shell's working directory. */
-			/* Alternative: try chdir inside a forked child: if(fork() == 0) { */
-			if (chdir(exec_argv[1]))
-				/* Error: change directory failed */
-				fprintf(stderr, "cd: failed to chdir %s\n", exec_argv[1]);	
-			/* End alternative: exit(EXIT_SUCCESS);} */
-
-		} else {
-		/* Execute Commands */
-			/* Try replacing 'fork()' with '0'.  What happens? */
-			pid_from_fork = fork();
-
-			if (pid_from_fork < 0) {
-				/* Error: fork() failed.  Unlikely, but possible (e.g. OS *
-				 * kernel runs out of memory or process descriptors).     */
-				fprintf(stderr, "fork failed\n");
-				continue;
-			}
-			if (pid_from_fork == 0) {
-				return imthechild(exec_argv[0], &exec_argv[0]);
-				/* Exit from main. */
-			} else {
-				imtheparent(pid_from_fork, run_in_background);
-				/* Parent will continue around the loop. */
-			}
-		} /* end if */
-	} /* end while loop */
-
-	return EXIT_SUCCESS;
-} /* end main() */
+            if (pid_from_fork < 0) {
+                fprintf(stderr, "fork failed\n");
+                continue;
+            }
+            if (pid_from_fork == 0) {
+                return imthechild(exec_argv[0], &exec_argv[0]);
+            } 
+            else {
+                imtheparent(pid_from_fork, run_in_background);
+            }
+        }
+        if (exec_argc > 0) {
+			command_counter++;
+        }
+    }
+    return EXIT_SUCCESS;
+}
